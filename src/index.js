8ee684d44ef05dc0cb18229ebb33f40c1b5f360b
Matt DeVoe <matthew.devoe@gmail.com>
2017-10-23 09:53:58 -0600

    struggled with click functionality, got to work by putting right in DOM listener

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const $ = require('jquery')
 const api = 'https://wordwatch-api.herokuapp.com'
+// let button = getElementsByTagName('button')
+
 
 document.addEventListener("DOMContentLoaded", () => {
   getTopWord()
+  $('button').on('click', function() {
+    debugger
+  })
 })
 
 
